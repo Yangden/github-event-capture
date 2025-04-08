c3db41797bd229a14cdfcc7c6d40bba8c5288e3d
Yangden <dengyang2001@outlook.com>
2025-04-08 14:38:13 -0400

    add query to read multiple records at onece

--- src/main/java/com/example/github_event_capture/repository/UserRepository.java
+++ src/main/java/com/example/github_event_capture/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.example.github_event_capture.repository;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.example.github_event_capture.entity.User;
-import java.util.Optional;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     // fetch a record by email
     User findByEmail(String email);
     User findById(long id);
+    // fetch a list of emails using a list of uids
+    @Query("SELECT u.email FROM Users u where u.uid IN :uidList")
+    List<String> fetchEmailsbyUids(@Param("uidList") List<Long> uids);
 }
