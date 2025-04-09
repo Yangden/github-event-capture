920f1e16e00e893b8fc9e4e97b84478436d21b5d
Yangden <dengyang2001@outlook.com>
2025-04-09 11:09:07 -0400

    debug the cusom query in user-repo: use jpa

--- src/main/java/com/example/github_event_capture/repository/UserRepository.java
+++ src/main/java/com/example/github_event_capture/repository/UserRepository.java
@@ -1,17 +1,17 @@
 package com.example.github_event_capture.repository;
 
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.github_event_capture.entity.User;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     // fetch a record by email
     User findByEmail(String email);
     User findById(long id);
     // fetch a list of emails using a list of uids
-    @Query("SELECT u.email FROM Users u where u.uid IN :uidList")
-    List<String> fetchEmailsbyUids(@Param("uidList") List<Long> uids);
+    @Query("SELECT u.email FROM User u where u.id IN ?1")
+    List<String> findEmailsByUids(List<Long> uids);
 }
