34002f4a13fcc795b64506997b363d7e5bf3fea0
Yangden <dengyang2001@outlook.com>
2025-04-09 11:10:30 -0400

    remove unnecessary annotations impacting repository test

--- src/main/java/com/example/github_event_capture/GithubEventCaptureApplication.java
+++ src/main/java/com/example/github_event_capture/GithubEventCaptureApplication.java
@@ -2,13 +2,13 @@ package com.example.github_event_capture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.example.github_event_capture.repository")
 @EnableAsync
 @EnableScheduling
 public class GithubEventCaptureApplication {
