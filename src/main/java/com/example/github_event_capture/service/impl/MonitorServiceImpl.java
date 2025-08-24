d8d0cb4b63c0eaf236936ed073729b7e56134fbe
Yangden <dengyang2001@outlook.com>
2025-08-24 10:45:40 -0400

    add monitor metrics on sqs message receiving rate

--- src/main/java/com/example/github_event_capture/service/impl/MonitorServiceImpl.java
+++ src/main/java/com/example/github_event_capture/service/impl/MonitorServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.github_event_capture.service.impl;
 
+import java.util.concurrent.ConcurrentHashMap;
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.stereotype.Service;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 
@@ -13,10 +15,17 @@ implement MonitorService interface
 @Service
 public class MonitorServiceImpl {
     private final PrometheusMeterRegistry prometheusMeterRegistry;
+    /* database read & write counters */
     private Counter mongodb_write_count;
     private Counter mongodb_read_count;
     private Counter postgres_write_count;
     private Counter postgres_read_count;
+    /* measure event sending rate in sqs producer */
+    private Counter event_count_afterDelete;
+    /* measure event rate at the receiver side of sqs */
+    private Counter event_count_sqsConsumer;
+    private Counter event_count_beforeDelete;
+
 
     public MonitorServiceImpl(PrometheusMeterRegistry prometheusMeterRegistry) {
         this.prometheusMeterRegistry = prometheusMeterRegistry;
@@ -41,6 +50,18 @@ public class MonitorServiceImpl {
                 .tags("metrics", "read-count")
                 .tags("db", "postgres")
                 .register(prometheusMeterRegistry);
+
+        this.event_count_sqsConsumer = Counter.builder("event.count")
+                .tags("metrics", "sqs_consumer")
+                .register(prometheusMeterRegistry);
+
+        this.event_count_beforeDelete = Counter.builder("event.count")
+                .tags("metrics", "sqs_receive")
+                .register(prometheusMeterRegistry);
+
+        this.event_count_afterDelete = Counter.builder("event.count")
+                .tags("metrics", "sqs_delete")
+                .register(prometheusMeterRegistry);
     }
 
     public void recordMongoDBWrite(double amount) {
@@ -58,5 +79,18 @@ public class MonitorServiceImpl {
     public void recordPostgresRead(double amount) {
         postgres_read_count.increment(amount);
     }
+
+    public void recordEventCountSqsConsumer(double amount) {
+        event_count_sqsConsumer.increment(amount);
+    }
+
+    public void recordEventCountBeforDelete(double amount) {
+        event_count_beforeDelete.increment(amount);
+    }
+
+    public void recordEventCountafterDelete(double amount) {
+        event_count_afterDelete.increment(amount);
+    }
+
 
 }
