55bb15fd75df2b11ec6cb9c9e762387bb66ffb20
Yangden <dengyang2001@outlook.com>
2026-05-22 09:55:18 -0400

    Add repository filter

--- src/main/java/com/example/github_event_capture/entity/dto/FiltersDTO.java
+++ src/main/java/com/example/github_event_capture/entity/dto/FiltersDTO.java
@@ -4,12 +4,20 @@ import java.util.HashSet;
 
 public class FiltersDTO {
     private HashSet<String> eventTypes;
+    private HashSet<String> repositories;
 
     public HashSet<String> getEventTypes() {
         return eventTypes;
     }
 
+    public HashSet<String> getRepositories() {
+        return repositories;
+    }
+
     public void setEventTypes(HashSet<String> eventTypes) {
         this.eventTypes = eventTypes;
+    }
+    public void setRepositories(HashSet<String> repositories) {
+        this.repositories = repositories;
     }
 }
